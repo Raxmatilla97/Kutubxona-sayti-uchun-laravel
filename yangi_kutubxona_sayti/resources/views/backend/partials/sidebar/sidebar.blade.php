3a1005ffb21e62038154246710f53f28032bd91b
wifixor97 <59994522+wifixor97@users.noreply.github.com>
2021-08-26 23:20:33 +0500

    Sidebar yangiladim

--- yangi_kutubxona_sayti/resources/views/backend/partials/sidebar/sidebar.blade.php
+++ yangi_kutubxona_sayti/resources/views/backend/partials/sidebar/sidebar.blade.php
@@ -3,7 +3,7 @@
 
      <div class="leftbar-user">
          <a href="javascript: void(0);">
-             <img src="{{ 'assets/images/users/avatar-1.jpg' }}" alt="user-image" height="42"
+             <img src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="user-image" height="42"
                  class="rounded-circle shadow-sm">
              <span class="leftbar-user-name">Soeng Souy</span>
          </a>
@@ -19,36 +19,22 @@
                  aria-controls="sidebarDashboards" class="side-nav-link">
                  <i class="uil-home-alt"></i>
                  <span class="badge bg-success float-end">4</span>
-                 <span> Dashboards </span>
+                 <span> Boshqaruv paneli </span>
              </a>
-             <div class="collapse" id="sidebarDashboards">
-                 <ul class="side-nav-second-level">
-                     <li>
-                         <a href="dashboard-analytics.html">Analytics</a>
-                     </li>
-                     <li>
-                         <a href="dashboard-crm.html">CRM</a>
-                     </li>
-                     <li>
-                         <a href="index.html">Ecommerce</a>
-                     </li>
-                     <li>
-                         <a href="dashboard-projects.html">Projects</a>
-                     </li>
-                 </ul>
-             </div>
+
          </li>
 
-         <li class="side-nav-title side-nav-item">Apps</li>
+
+         {{-- <li class="side-nav-title side-nav-item">Apps</li> --}}
 
          <li class="side-nav-item">
-             <a href="apps-calendar.html" class="side-nav-link">
+             <a href="{{ route('category.index') }}" class="side-nav-link">
                  <i class="uil-calender"></i>
-                 <span> Calendar </span>
+                 <span> Kataloglar </span>
              </a>
          </li>
 
-         <li class="side-nav-item">
+         {{-- <li class="side-nav-item">
              <a href="apps-chat.html" class="side-nav-link">
                  <i class="uil-comments-alt"></i>
                  <span> Chat </span>
@@ -90,7 +76,7 @@
                      </li>
                  </ul>
              </div>
-         </li>
+         </li> --}}
 
 
 
@@ -102,7 +88,7 @@
          <a href="javascript: void(0);" class="float-end close-btn text-body">
              <i class="mdi mdi-close"></i>
          </a>
-         <img src="{{ 'assets/images/help-icon.svg' }}" height="90" alt="Helper Icon Image">
+         <img src="{{ asset('assets/images/help-icon.svg') }}" height="90" alt="Helper Icon Image">
          <h5 class="mt-3">Unlimited Access</h5>
          <p class="mb-3">Upgrade to plan to get access to unlimited reports</p>
          <a href="javascript: void(0);" class="btn btn-outline-primary btn-sm">Upgrade</a>
