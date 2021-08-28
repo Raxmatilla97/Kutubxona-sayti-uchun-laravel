642b69cd7efd15ba7c134d498c63abd20a12b6c0
wifixor97 <59994522+wifixor97@users.noreply.github.com>
2021-08-28 19:57:48 +0500

    Kutubxonalarni manzillarini kiritish uchun yangi MCM

---
+++ yangi_kutubxona_sayti/app/Http/Controllers/KutubxonalarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kutubxonalar;
+use Illuminate\Http\Request;
+
+class KutubxonalarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kutubxonalar  $kutubxonalar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kutubxonalar $kutubxonalar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kutubxonalar  $kutubxonalar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kutubxonalar $kutubxonalar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kutubxonalar  $kutubxonalar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kutubxonalar $kutubxonalar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kutubxonalar  $kutubxonalar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kutubxonalar $kutubxonalar)
+    {
+        //
+    }
+}
