f83721837ea187395636072d08b6c41d21fd1200
wifixor97 <59994522+wifixor97@users.noreply.github.com>
2021-08-29 11:56:15 +0500

    Biroz to'g'irlandi

--- yangi_kutubxona_sayti/app/Http/Controllers/KutubxonalarController.php
+++ yangi_kutubxona_sayti/app/Http/Controllers/KutubxonalarController.php
@@ -14,7 +14,9 @@ class KutubxonalarController extends Controller
      */
     public function index()
     {
-        //
+        $kutubxonalar = Kutubxonalar::latest()->paginate(5);
+
+        return view('backend.pages.library-list', compact('kutubxonalar'))->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
@@ -24,7 +26,7 @@ class KutubxonalarController extends Controller
      */
     public function create()
     {
-        //
+        return view('backend.pages.library-create');
     }
 
     /**
@@ -35,7 +37,43 @@ class KutubxonalarController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'manzil' => 'required|max:255',
+            'img' => 'required|image|mimes:jpg,png,jpeg|max:1048',
+
+        ]);
+
+        $kutubxona = new Kutubxonalar();
+
+        $kutubxona->title = $request['title'];
+        $kutubxona->manzil = $request['manzil'];
+        $kutubxona->description = $request['description'];
+
+        // Manzilni avtomatik slug qiberadi
+        $kutubxona->slug = str_slug($request['title']);
+        $latestSlug = Kutubxonalar::whereRaw("slug RLIKE '^{$kutubxona->slug}(-[0-9]*)?$'")->latest('id')->value('slug');
+        if ($latestSlug) {
+            $pieces = explode('-', $latestSlug);
+            $number = intval(end($pieces));
+            $kutubxona->slug .= '-' . ($number + 1);
+            // cspi.uz/video
+            // cspi.uz/video1
+            // cspi.uz/video2
+        }
+        if ($image = $request->file('img')) {
+            $destinationPath = 'book_image/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $kutubimg['img'] = $destinationPath."$profileImage";
+
+
+        }
+              $kutubxona->img = $kutubimg['img'] ;
+
+        $kutubxona->save();
+
+        return redirect()->route('libraries.index')->with('success',"Siz kiritgan kutubxona ma'lumotlari bazaga kiritildi");
     }
 
     /**
